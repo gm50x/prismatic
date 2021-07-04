758d11056fda4ef56799c73d54890a394032ea2c
Getúlio Magela Silva <getuliomagela@outlook.com>
2021-07-04 15:31:05 -0300

    chore: adds a few published posts to the posts seed

--- prisma/posts.seed.ts
+++ prisma/posts.seed.ts
@@ -3,7 +3,8 @@ export const posts = [
     id: 1,
     title: 'Lorem Ipsum',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    authorId: 1
+    authorId: 1,
+    published: true
   },
   {
     id: 2,
@@ -15,7 +16,8 @@ export const posts = [
     id: 3,
     title: 'Consectetur Adipiscing Elit',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    authorId: 1
+    authorId: 1,
+    published: true
   },
   {
     id: 4,
